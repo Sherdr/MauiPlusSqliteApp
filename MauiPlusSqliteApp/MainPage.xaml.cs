6bca8e193925053aab0d96fdf60897e6e73b8cf3
Sherdr <rainar_kuper@mail.ru>
2025-08-10 08:40:45 +0300

    Separate ViewModel

--- MauiPlusSqliteApp/MainPage.xaml.cs
+++ MauiPlusSqliteApp/MainPage.xaml.cs
@@ -1,109 +1,13 @@
-﻿using MauiPlusSqliteApp.Data;
-using MauiPlusSqliteApp.Models;
-using MauiPlusSqliteApp.Services;
-using Microsoft.EntityFrameworkCore;
-using System.Collections.ObjectModel;
+﻿using MauiPlusSqliteApp.ViewModels;
 
 namespace MauiPlusSqliteApp
 {
     public partial class MainPage : ContentPage
     {
-
-        public ObservableCollection<Todo> TodoList { get; set; }
-
-        public MainPage(TodoRepository dbContext)
+        public MainPage()
         {
+            BindingContext = new MainViewModel();
             InitializeComponent();
-            TodoList = new ObservableCollection<Todo>();
-            BindingContext = this;
-            LoadTodoList();
-        }
-
-        async void LoadTodoList()
-        {
-            try
-            {
-                var todos = OperationsTodo.Read();
-                TodoList.Clear();
-                foreach(var todo in todos)
-                {
-                    TodoList.Add(todo);
-                }
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("Error!", ex.Message, "OK");
-            }
-        }
-
-        async void OnAddTodoClicked(object sender, EventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(TitleEntry.Text))
-            {
-                await DisplayAlert("Error!", "Title cannot be empty!", "OK");
-                return;
-            }
-            try
-            {
-                var todo = new Todo { Title = TitleEntry.Text };
-                OperationsTodo.Create(todo);
-                LoadTodoList();
-                TitleEntry.Text = string.Empty;
-                await DisplayAlert("Success!", "Todo added.", "OK");
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("Error!", ex.Message, "OK");
-            }
-        }
-
-        async void OnDeleteTodoClicked(object sender, EventArgs e)
-        {
-            if(sender is Button btn && btn.CommandParameter is Todo todoDelete)
-            {
-                bool confirmed = await DisplayAlert("Confirm Delete...",
-                    $"Are you sure want to delete {todoDelete.Title}",
-                    "Yes", "No");
-                if(!confirmed)
-                {
-                    return;
-                }
-                try
-                {
-                    OperationsTodo.Delete(todoDelete);
-                    LoadTodoList();
-                    await DisplayAlert("Success!", "Todo deleted.", "OK");
-                }
-                catch (Exception ex)
-                {
-                    await DisplayAlert("Error!", ex.Message, "OK");
-                }
-            }
-        }
-
-        async void OnEditTodoClicked(object sender, EventArgs e)
-        {
-            if(sender is Button btn && btn.CommandParameter is Todo todoEdit)
-            {
-                string newTitle = await DisplayPromptAsync("Edit title...",
-                    "Enter new title", initialValue: todoEdit.Title);
-                if (string.IsNullOrWhiteSpace(newTitle))
-                {
-                    await DisplayAlert("Error!", "Title cannot be empty!", "OK");
-                    return;
-                }
-                try
-                {
-                    todoEdit.Title = newTitle;
-                    OperationsTodo.Update(todoEdit);
-                    LoadTodoList();
-                    await DisplayAlert("Success!", "Todo updated.", "OK");
-                }
-                catch(Exception ex)
-                {
-                    await DisplayAlert("Error!", ex.Message, "OK");
-                }
-            }
         }
     }
 }
---
+++ MauiPlusSqliteApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,107 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MauiPlusSqliteApp.Models;
+using MauiPlusSqliteApp.Services;
+using System.Collections.ObjectModel;
+
+namespace MauiPlusSqliteApp.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        ObservableCollection<Todo> todoList;
+
+        [ObservableProperty]
+        string newTitle;
+
+        public MainViewModel()
+        {
+            TodoList = new ObservableCollection<Todo>();
+            LoadTodoList();
+        }
+
+        async void LoadTodoList()
+        {
+            try
+            {
+                var todos = OperationsTodo.Read();
+                TodoList.Clear();
+                foreach (var todo in todos)
+                {
+                    TodoList.Add(todo);
+                }
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+            }
+        }
+
+        [RelayCommand]
+        async void AddTodo()
+        {
+            if (string.IsNullOrWhiteSpace(NewTitle))
+            {
+                await Shell.Current.DisplayAlert("Error!", "Title cannot be empty!", "OK");
+                return;
+            }
+            try
+            {
+                var todo = new Todo { Title = NewTitle };
+                OperationsTodo.Create(todo);
+                LoadTodoList();
+                NewTitle = string.Empty;
+                await Shell.Current.DisplayAlert("Success!", "Todo added.", "OK");
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+            }
+        }
+
+        [RelayCommand]
+        async void EditTodo(Todo todoEdit)
+        {
+            string newTitle = await Shell.Current.DisplayPromptAsync("Edit title...",
+                    "Enter new title", initialValue: todoEdit.Title);
+            if (string.IsNullOrWhiteSpace(newTitle))
+            {
+                await Shell.Current.DisplayAlert("Error!", "Title cannot be empty!", "OK");
+                return;
+            }
+            try
+            {
+                todoEdit.Title = newTitle;
+                OperationsTodo.Update(todoEdit);
+                LoadTodoList();
+                await Shell.Current.DisplayAlert("Success!", "Todo updated.", "OK");
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+            }
+        }
+
+        [RelayCommand]
+        async void DeleteTodo(Todo todoDelete)
+        {
+            bool confirmed = await Shell.Current.DisplayAlert("Confirm Delete...",
+                    $"Are you sure want to delete {todoDelete.Title}",
+                    "Yes", "No");
+            if (!confirmed)
+            {
+                return;
+            }
+            try
+            {
+                OperationsTodo.Delete(todoDelete);
+                LoadTodoList();
+                await Shell.Current.DisplayAlert("Success!", "Todo deleted.", "OK");
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+            }
+        }
+    }
+}
